591b658566daf589998507b5fe9d7ddd14ffa2f3
Igor Iglesias <rogih_95@hotmail.com>
2023-03-10 13:01:22 +0100

    :sparkles: Create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img src="@/assets/images/logo.svg">
+        <input placeholder="Filter by name">
+    </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+    header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 2em;
+        background-color: var(--vt-c-black-soft);
+    }
+
+    img {
+        width: 350px;
+        height: 200px;
+    }
+</style>
