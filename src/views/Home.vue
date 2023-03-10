770e8afe7d533258a24614226333f651ef634879
Igor Iglesias <rogih_95@hotmail.com>
2023-03-10 13:01:57 +0100

    :sparkles: Call pokemon api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,13 @@
     <p>Hello World!</p>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import { pokemonsApi } from '@/services/api';
+
+    export default defineComponent({
+        async mounted() {
+            console.log(await pokemonsApi.getPokemons());
+        }
+    });
 </script>
