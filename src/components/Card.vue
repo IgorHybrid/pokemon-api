b52134e904d8deb74206a147792418d80a1acd64
Igor Iglesias <rogih_95@hotmail.com>
2023-03-11 00:27:47 +0100

    :sparkles: Add new components

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <h5>#{{ pokemon!["order"] }}</h5>
+    <h1> {{ pokemon!["name"] }}</h1>
+    <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon!['id']}.svg`"/>
+    <div>
+        <img v-for="elm in pokemon!['type']" :src="getImageURL(elm['name'])"/>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import type { PropType } from 'vue'
+    import type IPokemon from '@/models/pokemon.model'
+    import { getImageURL } from '@/utils';
+
+    export default defineComponent({
+        props: {
+            pokemon: Object as PropType<IPokemon>
+        },
+        methods: {
+            getImageURL
+        }
+    });
+</script>
---
+++ src/components/Filter.vue
@@ -0,0 +1,18 @@
+<template>
+    <label>{{ typeName }}</label>
+    <img :src="getImageURL(typeName)" :alt="typeName"/>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import { getImageURL } from '@/utils';
+
+    export default defineComponent({
+        props: {
+            typeName: String
+        },
+        methods: {
+            getImageURL
+        }
+    });
+</script>
