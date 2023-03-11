14ea77f42dff024107424745f7c3d4a4607a9bb6
Igor Iglesias <rogih_95@hotmail.com>
2023-03-11 18:09:25 +0100

    :sparkles: Add util to capitalize first letter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
-export const getImageURL = (typeName:string|undefined) => {
+export const getImageURL = (typeName:string) => {
     return new URL(`/src/assets/images/types/${typeName}.svg`, import.meta.url).href
+}
+
+export const capitalizeFirstLetter = (value:string) => {
+    return value[0].toUpperCase() + value.slice(1);
 }
