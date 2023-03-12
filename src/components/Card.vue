95f17672f762e3cd39709b760dc1b02ac000b8f8
Igor Iglesias <rogih_95@hotmail.com>
2023-03-12 15:19:29 +0100

    :lipstick: Add styles on hover

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="up">
-        <h4>#{{ pokemon!["order"] }}</h4>
-        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon!['id']}.svg`"/>
-    </div>
-    <p :style="{color: setDarkerColor(pokemon!['color'])}"> 
-        {{ pokemon!["name"].toUpperCase() }}
-    </p>
-    <div :class="pokemon!['type'].length > 1 ? 'two-type' : 'one-type'">
-        <img v-for="elm in pokemon!['type']" :src="getImageURL(elm['name'])"/>
+    <div class="container">
+        <div class="up">
+            <h4>#{{ pokemon!["order"] }}</h4>
+            <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon!['id']}.svg`"/>
+        </div>
+        <p :style="{color: setDarkerColor(pokemon!['color'])}"> 
+            {{ pokemon!["name"].toUpperCase() }}
+        </p>
+        <div :class="pokemon!['type'].length > 1 ? 'two-type' : 'one-type'">
+            <img v-for="elm in pokemon!['type']" :src="getImageURL(elm['name'])"/>
+        </div>
     </div>
 </template>
 
@@ -33,6 +35,36 @@
         text-align: center;
         font-weight: bold;
     }
+    .container {
+        min-width: 100%;
+        min-height: 100%;
+    }
+
+    .container:hover .up {
+        min-width: inherit;
+        min-height: inherit;
+    }
+
+    .container:hover .up img {
+        position: absolute;
+        width: 150px;
+        height: 150px;
+        top: -100%;
+        left: 20%;
+    }
+
+    .container:hover p {
+        position: absolute;
+        font-size: 18px;
+        top: 80%;
+        left: 10%;
+    }
+
+    .container:hover .one-type,
+    .container:hover .two-type {
+        top: 100%;
+    }
+
     .up img {
         position: relative;
         left: 25%;
