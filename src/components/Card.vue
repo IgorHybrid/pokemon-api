1219ef269bf6cb098f51b00d3f09a6f0705368de
Igor Iglesias <rogih_95@hotmail.com>
2023-03-12 01:35:15 +0100

    :recycle: Change structure of elements

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,12 @@
 <template>
-    <h5>#{{ pokemon!["order"] }}</h5>
-    <h1> {{ pokemon!["name"].toUpperCase() }}</h1>
+    <div class="up">
+        <h4>#{{ pokemon!["order"] }}</h4>
     <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon!['id']}.svg`"/>
-    <div>
+    </div>
+    <p :style="{color: setDarkerColor(pokemon!['color'])}"> 
+        {{ pokemon!["name"].toUpperCase() }}
+    </p>
+    <div :class="pokemon!['type'].length > 1 ? 'two-type' : 'one-type'">
         <img v-for="elm in pokemon!['type']" :src="getImageURL(elm['name'])"/>
     </div>
 </template>
@@ -11,14 +15,15 @@
     import { defineComponent } from 'vue';
     import type { PropType } from 'vue'
     import type IPokemon from '@/models/pokemon.model'
-    import { getImageURL } from '@/utils';
+    import { getImageURL, setDarkerColor } from '@/utils';
 
     export default defineComponent({
         props: {
             pokemon: Object as PropType<IPokemon>
         },
         methods: {
-            getImageURL
+            getImageURL,
+            setDarkerColor
         }
     });
 </script>
